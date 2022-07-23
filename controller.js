6df988cc9919808180794928b1873566ce7b2cbf
Abhijith-S <abiarunodayam@gmail.com>
2022-07-23 20:43:50 +0530

    pushing...

--- controller.js
+++ controller.js
@@ -1,7 +1,7 @@
 angular.module('weatherApplication').controller('WeatherAppController',['$scope','$http','WeatherAppService',
     function($scope,$http, WeatherAppService){
         angular.element(function(){
-            $scope.error = false
+            $scope.error = false;
         });
 
         function sleep(ms) {
@@ -15,19 +15,19 @@ angular.module('weatherApplication').controller('WeatherAppController',['$scope'
             array = $scope.SelectStateList()
             $scope.TempList = []
             array.forEach(element => {
-                $scope.getData(element, $scope.SelectCountry)
+                $scope.getData(element)
                 sleep(100)
 
             });
             console.log($scope.TempList)
         }
         //lists of major cities in each specified countries
-        var stateListIndia = ['Mumbai', 'Delhi','Banglore','Hyderabad','Ahmedabad','Chennai','Kolkata','Kochi','Thiruvanathapuram', 'Pune', 'Surat', 'Lucknow','Ghaziabad', 'Vijayawada','Faridabad', 'Meerut','Navi Mumbai', 'Howrah', 'Coimbatore', 'Jodhpur', 'Madurai', 'Mysore', 'Gurgaon', 'Mangalore', 'Kozhikode','Agartala', 'Thrissur', 'Pondicherry' ]
+        var stateListIndia = ['Mumbai', 'Delhi','bengaluru','Hyderabad','Ahmedabad','Chennai','Kolkata','Kochi','Trivandrum', 'Pune', 'Surat', 'Lucknow','Ghaziabad', 'Vijayawada','Faridabad', 'Meerut','Navi Mumbai', 'Howrah', 'Coimbatore', 'Jodhpur', 'Madurai', 'Mysore', 'Gurgaon', 'Mangalore', 'Kozhikode','Agartala', 'Thrissur', 'Pondicherry' ]
         var stateListUSA = ['New York City', 'Los Angeles','Chicago','Houston','Phoenix','Philadelphia','San Antonio','San Diego','Dallas','San Jose','Austin','Jacksonville','Fort Worth','Columbus','	Charlotte','Indianapolis','San Francisco', 'Seattle','Denver','Washington','Nashville','Oklahoma City','Boston','El Paso','Portland','Las Vegas','Memphis','Detroit','Baltimore','Milwaukee']
-        var stateListUK = ['London','Birmingham','Leeds','Glasgow','Sheffield','Bradford','Liverpool','Edinburgh','Manchester','Bristol', 'Kirklees','Fife','Wirral','North Lanarkshire','Wakefield','Cardiff','Dudley','Wigan','East Riding','South Lanarkshire','Coventry','Belfast','Leicester','Sunderland','Sandwell','Doncaster','Stockport','Sefton','Nottingham']
+        var stateListUK = ['London','Birmingham','Leeds','Glasgow','Sheffield','Bradford','Liverpool','Edinburgh','Manchester','Bristol', 'Kirklees','Fife','Wirral','North Lanarkshire','Wakefield','Cardiff','Dudley','Wigan','South Lanarkshire','Coventry','Belfast','Leicester','Sunderland','Sandwell','Doncaster','Stockport','Sefton','Nottingham']
         var statelistGermany = ['Berlin','Hamburg','Munich','Cologne','Frankfurt am Main','Stuttgart','Dortmund',' Essen','Leipzig','Bremen','Dresden','Hanover','Nuremberg','Duisburg','Bochum','Wuppertal','Bielefeld','Bonn','Karlsruhe','Mannheim','Augsburg','Wiesbaden','Gelsenkirchen','Braunschweig','Chemnitz','Kiel','Aachen']
-        var statelistUAE = ['Dubai','Abu Dhabi','Sharjah','Al Ain','Ajman','Ras Al Khaimah','Fujairah','Umm al-Quwain','Khor Fakkan','Kalba','Jebel Ali','Madinat Zayed','Ruwais','Liwa Oasis','Dhaid','Ghayathi','Dibba Al-Hisn','Hatta','Al Madam','Adhen','Dalma','Dadna','Huwaylat','Marawah','Mleiha','Sila','Masfut','Digdaga','Asimah']
-        console.log(statelistUAE.length,stateListUSA.length)
+        var statelistFrance = []
+        console.log(stateListUSA.length)
         
         
         
@@ -56,13 +56,14 @@ angular.module('weatherApplication').controller('WeatherAppController',['$scope'
 
         //get weather data
        
-        $scope.getData = function(city, country){
+        $scope.getData = function(city){
             try{
-                WeatherAppService.getWeather(city, country).then(function(response){
+                WeatherAppService.getWeather(city).then(function(response){
                 $scope.weatherData = response
                     try{
-                        console.log($scope.weatherData)
-                        var Items = {item:[$scope.weatherData.data[0].city_name, $scope.weatherData.data[0].app_temp]}
+                        // console.log($scope.weatherData)
+                        var Items = {item:[$scope.weatherData.name,$scope.weatherData.main.temp ]}
+                        console.log(Items)
                     // /console.log($scope.weatherData.data[0].city_name, $scope.weatherData.data[0].app_temp)
                         $scope.TempList.push(Items)
                     }
@@ -76,6 +77,26 @@ angular.module('weatherApplication').controller('WeatherAppController',['$scope'
                 $scope.error = true;
             }
 
+        }
+        $scope.getcity = function(country){
+            $scope.abhi=[];
+            try{
+                WeatherAppService.post(country)
+                .then(data => {
+                    data.data.forEach(function(post){
+                        // console.log(post,"----")
+                        $scope.abhi.push(post);
+                        console.log("man   ::: "+$scope.abhi); 
+                    }) ;
+                })
+                .catch(err => console.log(err));
+            }
+            catch(e){
+                console.log(e)
+                $scope.error = true;
+            }
+
+           
         }
 
 }])
--- services.js
+++ services.js
@@ -2,12 +2,15 @@ angular.module('weatherApplication').service('WeatherAppService',['$http','$q',
 function($http, $q){
     var WeatherAppService = {
         // give functions
-        getWeather:getWeather
+        getWeather:getWeather,
+        getCities:getCities,
+        post:post
     };
     return WeatherAppService
 
-    function getWeather(city, country){
-        var baseURL = "https://api.weatherbit.io/v2.0/current?city="+city+"&country="+country+"&key=25ddacfe8f8a405aac37bad9066da07d"
+    function getWeather(city){
+        var baseURL = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=bf0b89712c52d65d5351033913e3d2e0&units=metric"
+        // var baseURL = "https://api.weatherbit.io/v2.0/current?city="+city+"&country="+country+"&key=25ddacfe8f8a405aac37bad9066da07d"
         var def = $q.defer();
         var config = {methord: "GET", headers:{'Content-Type':"application/json ; charset=UTF-8"}};
         $http.get(baseURL, config).success(function(response){
@@ -19,6 +22,61 @@ function($http, $q){
     
     
     };
+
+    function getCities(country){  
+        var def = $q.defer();
+        var config = {methord: "POST", headers:{'Content-Type':"application/json ; charset=UTF-8"}, body:{'country':country}};
+        $http.post(BaseUrl, config).success(function(response){
+            def.resolve(response);
+        }).error(function(){
+            def.reject({error:"error"})
+        });
+        return def.promise
+        
+
+    }
+
+    //  function post(country) {
+    //         var BaseUrl = "https://countriesnow.space/api/v0.1/countries/cities"
+    //          return  new Promise((resolve, reject) => {
+    //           fetch(BaseUrl, {
+    //             method: 'POST',
+    //             headers: {
+    //               'Content-type': 'application/json'
+    //             },
+    //             body: JSON.stringify({'country':country})
+    //           })
+    //           .then(res => res.json())
+    //           .then(data => resolve(data))
+    //           .catch(err => reject(err));
+    //         });
+    // }
+
+    async function  post(country) {
+        var BaseUrl = "https://countriesnow.space/api/v0.1/countries/cities"
+        const response = await fetch(BaseUrl, {
+          method: 'POST',
+          headers: {
+            'Content-type': 'application/json'
+          },
+          body: JSON.stringify({'country':country})
+        });
+    
+        const resData = await response.json();
+        return resData;
+       
+      }
+
+
+
+    // function getCities() {
+    //     const response =  fetch(`http://api.wunderground.com/api/${this.apiKey}/conditions/q/${this.state}/${this.city}.json`).then();
+    
+    //     const responseData =  response.json();
+    
+    //     return responseData.current_observation;
+    //   }
+
 
 
 
